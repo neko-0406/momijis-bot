95c006e7457d90a94131942f7810dc4854eace6f
momiji <sateraito3696@gmail.com>
2026-03-23 11:47:48 +0900

    botの起動まで実装

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import { Client, GatewayIntentBits } from "discord.js";
+import config from "./bot_config.json";
+
+const intents = [
+  GatewayIntentBits.MessageContent,
+  GatewayIntentBits.GuildMessages
+]
+
+const client = new Client({ intents: intents });
+
+client.on('clientReady', event => {
+  console.log("bot is ready...");
+  console.log(event.user.displayName);
+})
+
+client.login(config.token);
+
